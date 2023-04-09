fe33953cf486566319ab08da44636011a836d91c
Crowdbotics <engineering@crowdbotics.com>
2023-04-09 00:22:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/super_morning_dev_75443/MainActivity.java
@@ -0,0 +1,15 @@
+package com.super_morning_dev_75443;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "super_morning_dev_75443";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-morning-dev-75443",
+  url: "https://super-morning-dev-75443.botics.co",
+  api: "https://super-morning-dev-75443.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
